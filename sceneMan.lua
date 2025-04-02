a602ab613f4bcf49e337a65acdac32279982d143
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2025-04-02 21:47:15 +0100

    Add EmmyLua style comment to sceneMan.lua

--- sceneMan.lua
+++ sceneMan.lua
@@ -1,16 +1,25 @@
+---@class SceneManager
 local sceneMan = {
-    scenes = {}, -- All created scenes will be stored here.
-    stack = {}, -- Scenes that are pushed will be stored here.
-    shared = {}, -- Stores variables that are shared between scenes
-    saved = {}, -- Stores saved stacks so they can be restored later
-    buffer = {}, -- Stores the scene stack when the original scene stack is disabled
-    frozen = false, -- If true, the buffer will be used instead of the original stack
-    lockLevel = 0, -- They highest level of the stack that is locked
-    version = "1.4.1", -- The used version of Scene Man
+    ---@type table<string, table> All created scenes will be stored here.
+    scenes = {},
+    ---@type table<integer, table> Scenes that are pushed will be stored here.
+    stack = {},
+    ---@type table<any, any> Stores variables that are shared between scenes.
+    shared = {},
+    ---@type table<string, table> Stores saved stacks so they can be restored later.
+    saved = {},
+    ---@type table<integer, table> Stores the scene stack when the original scene stack is disabled.
+    buffer = {},
+    ---@type boolean If true, the buffer will be used instead of the original stack.
+    frozen = false,
+    ---@type integer The highest level of the stack that is locked.
+    lockLevel = 0,
+    ---@type string The version of Scene Manager being used.
+    version = "1.4.1",
 }
 
---- A helper funciton that returns either the buffer or the stack based on the value of menuMan.frozen.
--- @return (table) The buffer if the frozen flag is true, other the stack
+--- Returns either the buffer or the stack based on the value of `sceneMan.frozen`.
+---@return table<integer, table> sceneArr The buffer if the frozen flag is true, otherwise the stack.
 local function getStack ()
     return (sceneMan.frozen == true) and sceneMan.buffer or sceneMan.stack
 end
@@ -28,7 +37,7 @@ function sceneMan:freeze ()
     end
 end
 
---- Copies the changes from the buffer back into the original stack.
+--- Copies changes from the buffer back into the original stack.
 function sceneMan:unfreeze ()
     if self.frozen == true then
         self.stack = {} -- Resets the stack
@@ -45,6 +54,11 @@ end
 -- This will save the frozen buffer if the stack is frozen
 -- This will not modify the current stack in any way
 -- @param id (string) A unique ID that will be used to identify the saved stack. It will override anything currently stored at that ID
+
+--- Saves the current contents of the stack so it can be restored later.
+--- This will save the frozen buffer if the stack is frozen.
+--- This will not modify the current stack in any way.
+---@param id string A unique ID to identify the saved stack. Overrides any existing entry at this ID.
 function sceneMan:saveStack (id)
     local stack = getStack ()
     local savedStack = {}
@@ -61,6 +75,12 @@ end
 -- @param id (string) A unique ID that identifies the stack that should be restored
 -- @param ... (varargs) A list of values that will be passed to the event's "whenAdded" callback function
 -- @return (bool) True if the stored stack at the given ID exists and if the current stack is empty, otherwise false
+
+--- Restores a saved stack from storage.
+--- This will call the loaded scenes' "whenAdded" methods.
+---@param id string A unique ID identifying the stack to restore.
+---@vararg any Values passed to the "whenAdded" callback function of scenes.
+---@return boolean success True if restoration succeeded (stack exists and current stack is empty), false otherwise.
 function sceneMan:restoreStack (id, ...)
     local stack = getStack ()
     local savedStack = self.saved[id]
@@ -76,38 +96,36 @@ function sceneMan:restoreStack (id, ...)
     end
 end
 
---- Removes a saved stack permanently.
--- This will not delete the scenes in the stack
--- This will not affect the current stack, even if it was restored using the to-be-deleted stack
--- @param id (string) A unique ID that identifies the stack that should be deleted
+--- Deletes a saved stack permanently.
+--- Does not affect scenes in the current stack, even if it was restored using the to-be-deleted stack.
+--- This will not *delete* the scenes in the stack.
+---@param id string A unique ID identifying the saved stack to delete.
 function sceneMan:deleteStack (id)
     self.saved[id] = nil
 end
 
---- Locks the stack up until the specified level.
--- Locked scenes will have their event callbacks skipped, except for their "whenAdded", "whenRemoved", or "deleted" methods
--- The bottommost item of the stack is at level 1
--- @param level (int) The level that the stack should be locked up to
+--- Locks the stack up to a specified level.
+--- Locked scenes skip their event callbacks except "whenAdded", "whenRemoved", or "deleted".
+---@param level integer The level to lock up to (bottommost item is at level 1).
 function sceneMan:lock (level)
     self.lockLevel = level
 end
 
---- Unlocks the stack, which will allow all scenes to execute their event callbacks again.
+--- Unlocks all levels in the stack, allowing all scenes to execute their event callbacks again.
 function sceneMan:unlock ()
     self.lockLevel = 0
 end
 
---- Gets the current lock level.
--- @return (int) The current lock level
+--- Gets the current lock level of the stack.
+---@return integer level The current lock level.
 function sceneMan:getLockLevel ()
     return self.lockLevel
 end
 
---- Adds a new scene to Scene Man and initializes it via its load method.
--- This will call the scene's "load" method
--- @param name (string) The name of the new scene. This will be used later to push, insert, and remove this scene from the stack
--- @param scene (table) A table containing the scene's attributes and callback functions
--- @param ... (varargs) A series of values that will be passed to the scene's "load" callback
+--- Adds a new scene and initializes it via its `load` method.
+---@param name string Name of the new scene (used for later operations like push/remove).
+---@param scene table Table containing attributes and callback functions of the scene.
+---@vararg any Values passed to the `load` callback function of this scene.
 function sceneMan:newScene (name, scene, ...)
     self.scenes[name] = scene
     self.scenes[name].name = name
@@ -116,11 +134,9 @@ function sceneMan:newScene (name, scene, ...)
     end
 end
 
---- Removes a scene from Scene Man and calls its delete method.
--- This will call the scene's "delete" method
--- If you try to push or insert a deleted scene, Scene Man will throw an error!
--- @param name (string) The name of the scene that should be deleted
--- @param ... (varargs) A series of values that will be passed to the scene's "delete" callback
+--- Deletes a registered scene and calls its `delete` method. Deleted scenes cannot be pushed or inserted again.
+---@param name string Name of the scene to delete.
+---@vararg any Values passed to the `delete` callback function of this scene.
 function sceneMan:deleteScene (name, ...)
     if self.scenes[name] ~= nil then
         if self.scenes[name].delete ~= nil then
@@ -130,25 +146,22 @@ function sceneMan:deleteScene (name, ...)
     end
 end
 
---- Returns the current size of the stack.
--- @return (int) The size of the stack
+--- Gets the current size of the stack.
+---@return integer size The size of the stack (number of scenes).
 function sceneMan:getStackSize ()
     return #self.stack
 end
 
---- Gives the name of the current scene. It will return nil is there are no scenes on the stack.
--- This will not look inside the frozen buffer, even if the stack is frozen
--- @return (string) The name of the scene at the top of the stack or nil if the stack is empty
+--- Gets the name of the current topmost scene on the stack. Returns nil if no scenes exist in it (ignores frozen buffer).
+---@return string|nil sceneName Name of topmost scene or nil if empty.
 function sceneMan:getCurrentScene ()
     return (#self.stack >= 1) and self.stack[#self.stack].name or nil
 end
 
---- Adds a scene from the scenes table onto the stack.
--- This will call the scene's "whenAdded" method
--- Scenes at the top of the stack will have their functions called last
--- @raise When the given scene name isn't registered inside Scene Man
--- @param name (string) The name of the scene to add to the top of the stack
--- @param ... (varargs) A list of values that will be passed to the event's "whenAdded" callback function
+--- Pushes a registered scene onto the top of the stack and calls its `whenAdded` method.
+--- Scenes at the top of the stack will have their functions called last
+---@param name string Name of registered scene to push onto stack.
+---@vararg any Values passed to `whenAdded` callback function of this scene.
 function sceneMan:push (name, ...)
     local stack = getStack ()
     
@@ -163,8 +176,8 @@ function sceneMan:push (name, ...)
 end
 
 --- Pops a scene off of the stack.
--- This will call the topmost scene's "whenRemoved" method
--- @param ... (varargs) A list of values that will be passed to the event's "whenRemoved" callback function
+--- This will call the topmost scene's `whenRemoved` method.
+---@vararg any Values passed to the `whenRemoved` callback function of this scene.
 function sceneMan:pop (...)
     local stack = getStack ()
     
@@ -178,12 +191,11 @@ function sceneMan:pop (...)
 end
 
 --- Adds a scene to the stack at a given index.
--- This will call the scene's "whenAdded" method
--- @raise When the given scene name isn't registered inside Scene Man
--- @param name (string) The name of the scene to add to the top of the stack
--- @param index (int) The position within the stack that the scene should be inserted at
--- @param ... (varargs) A list of values that will be passed to the event's "whenAdded" callback function
--- @return (bool) True if the operation was successful
+--- This will call the scene's `whenAdded` method.
+---@param name string The name of the scene to add to the stack.
+---@param index integer The position within the stack that the scene should be inserted at.
+---@vararg any Values passed to the `whenAdded` callback function of this scene.
+---@return boolean success True if the operation was successful, false otherwise.
 function sceneMan:insert (name, index, ...)
     local stack = getStack ()
     
@@ -202,10 +214,10 @@ function sceneMan:insert (name, index, ...)
 end
 
 --- Removes a scene from the stack at a certain index.
--- This will call the scene's "whenRemoved" method
--- @param index (int) The position within the stack that the scene should be removed at
--- @param ... (varargs) A list of values that will be passed to the event's "whenRemoved" callback function
--- @return (bool) True if the operation was successful
+--- This will call the scene's `whenRemoved` method.
+---@param index integer The position within the stack that the scene should be removed at.
+---@vararg any Values passed to the `whenRemoved` callback function of this scene.
+---@return boolean success True if the operation was successful, false otherwise.
 function sceneMan:remove (index, ...)
     local stack = getStack ()
     
@@ -219,9 +231,9 @@ function sceneMan:remove (index, ...)
 end
 
 --- Removes all scenes from the stack, starting at the top.
--- This will call all the scenes' "whenRemoved" methods, starting from the topmost scene
--- This will automatically freeze the stack until all scenes have been iterated over
--- @param ... (varargs) A list of values that will be passed to the event's "whenRemoved" callback function
+--- This will call all the scenes' `whenRemoved` methods, starting from the topmost scene.
+--- This will automatically freeze the stack until all scenes have been iterated over.
+---@vararg any Values passed to the `whenRemoved` callback function of this scene.
 function sceneMan:clearStack (...)
     local prefrozen = self.frozen
     self:freeze ()
@@ -239,9 +251,9 @@ function sceneMan:clearStack (...)
 end
 
 --- Removes all scenes from the unlocked portion of the stack, starting at the top.
--- This will call all the scenes' "whenRemoved" methods, starting from the topmost scene
--- This will automatically freeze the stack until all scenes have been iterated over
--- @param ... (varargs) A list of values that will be passed to the event's "whenRemoved" callback function
+--- This will call all the scenes' `whenRemoved` methods, starting from the topmost scene.
+--- This will automatically freeze the stack until all scenes have been iterated over.
+---@vararg any Values passed to the `whenRemoved` callback function of this scene.
 function sceneMan:clearUnlockedStack (...)
     local prefrozen = self.frozen
     self:freeze ()
@@ -259,9 +271,9 @@ function sceneMan:clearUnlockedStack (...)
 end
 
 --- Fires an event callback for all scenes on the stack.
--- This will automatically freeze the stack until all scenes have been iterated over
--- @param eventName (string) The name of the event
--- @param ... (varargs) A series of values that will be passed to the scenes' event callbacks
+--- This will automatically freeze the stack until all scenes have been iterated over.
+---@param eventName string The name of the event.
+---@vararg any Values passed to the scenes' event callbacks.
 function sceneMan:event (eventName, ...)
     local prefrozen = self.frozen
     self:freeze ()
